0b4da34f232845d16345bbe096bce86e0ac34872
thayer11 <holly.coddington@gmail.com>
2016-08-14 21:34:39 -0600

    almost finished challenge 3

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,24 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("CohortController", CohortController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+	this.full_name = "Holly Coddington"
+	this.age = 33;
+	this.city = "Denver";
+	this.state = "CO."
+	this.shout = function(string){
+    	return string.toUpperCase() + "!";
+  }
+}
+
+function CohortController(){
+	this.class_name = "White Walkers"
+	this.enrolled_students = ["Allison", "Greg", "Kyle", "Jamie", "Maddy", "Kayce", "Nick", "Mark", "Charlie", "Monica", "Louie", "Holly", "Matt", "Abe"];
+	this.start_date = "6-13-2016"
+	this.end_date = "9-02-2016"
+	this.days_remaining = "21"
+}
+
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,25 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController);
+app.controller("loanController", loanController);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+	this.name = "Holly Coddington"; 
+	this.height = 5.6; 
+{
+
+ function loanController(){
+ 	this.amount = "10000";
+ 	this.apr = "6.8";
+
+ this.month = function(n){
+ 	var annual_interest = (this.amount*(this.apr/100));
+ 	var monthly_interest = (annual_interest / 12);
+ 	var accrued_interest = monthly_interest * n || 0;
+ 		return{
+ 			accrued_interest: accured_interest,
+ 			total_balance: this.amount + accrued_interest
+ 		}
+	}
+}
